76989a06ffe2171e377df1df193242040543483e
Kilerd <blove694@gmail.com>
2021-01-26 08:16:29 +0800

    fix: add offset for get update to avoid duplicated incoming messages.

--- src/bot.rs
+++ src/bot.rs
@@ -3,11 +3,11 @@ use std::future::Future;
 use std::sync::Arc;
 
 use crate::error::{ApiResult, BotResult};
-use crate::TelegramApiMethod;
-use crate::typing::Update;
 use crate::method::webhook::GetUpdates;
-use tokio::time::Duration;
+use crate::typing::Update;
+use crate::TelegramApiMethod;
 use std::convert::TryInto;
+use tokio::time::Duration;
 
 pub struct Bot {
     pub secret_key: String,
@@ -24,16 +24,21 @@ impl Bot {
     }
 
     pub async fn request<T: TelegramApiMethod>(&self, method: T) -> BotResult<T::Response> {
-        let result = surf::post(format!("https://api.telegram.org/bot{}/{}", self.secret_key, method.get_method()))
-            .body(surf::Body::from_json(&method).unwrap())
-            .await;
+        let result = surf::post(format!(
+            "https://api.telegram.org/bot{}/{}",
+            self.secret_key,
+            method.get_method()
+        ))
+        .body(surf::Body::from_json(&method).unwrap())
+        .await;
         let x = result.unwrap().body_json::<ApiResult<T::Response>>().await;
         x.unwrap().into_result()
     }
 
-    pub fn command<H, F>(&mut self, command: &str, handler: H) where
+    pub fn command<H, F>(&mut self, command: &str, handler: H)
+    where
         H: (Fn(Arc<Bot>, Arc<Update>) -> F) + Send + Sync + 'static,
-        F: std::future::Future<Output=()> + Send + 'static
+        F: std::future::Future<Output = ()> + Send + 'static,
     {
         self.commands
             .entry(command.to_string())
@@ -52,19 +57,23 @@ impl Bot {
     }
 
     pub async fn run(self) {
-
         let arc_self = Arc::new(self);
 
         let mut interval1 = tokio::time::interval(Duration::from_millis(100));
         loop {
             interval1.tick().await;
-            let result = arc_self.request(GetUpdates {
-                offset: None,
-                limit: None,
-                timeout: None,
-                allowed_updates: None
-            }).await.unwrap();
+            let mut offset = 0;
+            let result = arc_self
+                .request(GetUpdates {
+                    offset: if offset == 0 { None } else { Some(offset) },
+                    limit: None,
+                    timeout: Some(10),
+                    allowed_updates: None,
+                })
+                .await
+                .unwrap();
             for one_msg in result {
+                offset = one_msg.update_id;
                 let msg = Arc::new(one_msg);
                 let option = arc_self.commands.get("HELP");
                 if let Some(handlers) = option {
@@ -83,28 +92,28 @@ mod test {
     use crate::method::GetMe;
     use crate::typing::{Update, UpdateMessage};
     use std::sync::Arc;
-//
-//    #[tokio::test]
-//    async fn test_binding_server() {
-//        let bot = Bot::new();
-//        let result = bot.request(GetMe {}).await;
-//        dbg!(result);
-//    }
-//
-//    #[tokio::test]
-//    async fn handle_a_new_command() {
-//        let mut bot = Bot::new();
-//        let x = |bbot:Arc<Bot>, update:Arc<Update>| {
-//            async move {
-//                println!("pong");
-//                let result = bbot.request(GetMe {}).await;
-//                dbg!(result);
-//            }
-//        };
-//        bot.command("ping", x);
-//        let update1 = Update { update_id: 0, message: UpdateMessage::Unknown };
-//        let arc = Arc::new(bot);
-//
-//        bot.handle(arc, update1).await;
-//    }
+    //
+    //    #[tokio::test]
+    //    async fn test_binding_server() {
+    //        let bot = Bot::new();
+    //        let result = bot.request(GetMe {}).await;
+    //        dbg!(result);
+    //    }
+    //
+    //    #[tokio::test]
+    //    async fn handle_a_new_command() {
+    //        let mut bot = Bot::new();
+    //        let x = |bbot:Arc<Bot>, update:Arc<Update>| {
+    //            async move {
+    //                println!("pong");
+    //                let result = bbot.request(GetMe {}).await;
+    //                dbg!(result);
+    //            }
+    //        };
+    //        bot.command("ping", x);
+    //        let update1 = Update { update_id: 0, message: UpdateMessage::Unknown };
+    //        let arc = Arc::new(bot);
+    //
+    //        bot.handle(arc, update1).await;
+    //    }
 }
